3abbe972cbddcc5b0273217363a209704c465dae
Tobias Keller <kellertobs@gmail.com>
2021-10-07 12:11:54 +0200

    update run scripts

--- usr/run_fract.m
+++ usr/run_fract.m
@@ -1,11 +1,11 @@
 clear; close all;
 
 % set run parameters
-runID    =  'test';          % run identifier
+runID    =  'fract_upw2';        % run identifier
 restart  =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
-nop      =  1;                  % output frame plotted/saved every 'nop' time steps
+nop      =  50;                  % output frame plotted/saved every 'nop' time steps
 plot_op  =  1;                   % switch on (1) to live plot results
-save_op  =  0;                   % switch on (1) to save output to file
+save_op  =  1;                   % switch on (1) to save output to file
 plot_cv  =  1;                   % switch on (1) to live plot iterative convergence
 isotherm =  0;                   % switch on (1) isothermal mode
 isochem  =  0;                   % switch on (1) isochemical mode
@@ -19,34 +19,34 @@ h        =  D/(N-2);             % grid spacing (equal in both dimensions, do no
 dw       =  h;                   % boundary layer thickness for cooling/outgassing/assimilation [m]
 
 % set model timing parameters
-M        =  1e5;                 % number of time steps to take
-tend     =  3600*24;             % end time for simulation [s]
-dt       =  10;                   % initial time step [s]
+M        =  2e4;                 % number of time steps to take
+tend     =  3600*120;            % end time for simulation [s]
+dt       =  10;                  % initial time step [s]
 hr       =  3600;                % conversion seconds to hours
 
 % set model vesicularity parameters
-ftop     =  nan;                 % top outgassing vesicularity [vol] (nan = no outgassing)
+ftop     =  0.02;                % top outgassing vesicularity [vol] (nan = no outgassing)
 tau_f    =  1*hr;                % top outgassing time [s]
 kf       =  1e-9;                % volume diffusivity [m^2/s]
 smth     =  (N/25)^2;            % regularisation of initial random perturbation
-zlay     =  0.9;                 % layer thickness
+zlay     =  0.1;                 % layer thickness
 
 % set model thermo-chemical parameters
-T0       =  710;                % temperature top layer [wt SiO2]
-T1       =  710;                % temperature base layer [wt SiO2]
-dT       =  1;                   % amplitude of random noise [wt SiO2]
-c0       =  0.56;                % major component top layer [wt SiO2]
-c1       =  0.56;                % major component base layer [wt SiO2]
-dc       =  0.01;                   % amplitude of random noise [wt SiO2]
-v0       =  0.01;                % volatile component top layer [wt H2O]
-v1       =  0.01;                % volatile component base layer [wt H2O]
-dv       =  0;                   % amplitude of random noise [wt H2O]
+T0       =  1200;                % temperature top layer [wt SiO2]
+T1       =  1200;                % temperature base layer [wt SiO2]
+dT       =  0;                   % amplitude of random noise [wt SiO2]
+c0       =  0.50;                % major component top layer [wt SiO2]
+c1       =  0.50;                % major component base layer [wt SiO2]
+dc       =  0.001;               % amplitude of random noise [wt SiO2]
+v0       =  0.02;                % volatile component top layer [wt H2O]
+v1       =  0.02;                % volatile component base layer [wt H2O]
+dv       =  0.001;               % amplitude of random noise [wt H2O]
 Ptop     =  1e8;                 % top pressure [Pa]
 ctop     =  nan;                 % top composition [wt% SiO2] (nan = no assimilation)
 tau_c    =  48*hr;               % chamber wall assimilation time [s]
 Twall    =  500;                 % wall temperature [degC] (nan = insulating)
-coolmode =  3;                   % mode of wall cooling (0 = no cooling; 1 = top only; 2 = top/bot only; 3 = all walls)
-tau_T    =  24*hr;               % chamber wall cooling time [s]
+coolmode =  2;                   % mode of wall cooling (0 = no cooling; 1 = top only; 2 = top/bot only; 3 = all walls)
+tau_T    =  12*hr;               % chamber wall cooling time [s]
 kc       =  1e-7;                % major component diffusivity [m^2/s]
 kv       =  1e-7;                % volatile component diffusivity [m^2/s]
 kTm      =  4;                   % melt thermal diffusivity [m2/s]
@@ -74,15 +74,15 @@ DLf      =  400e3;               % latent heat [J/kg]
 % set model trace and isotope geochemistry parameters
 it0      =  1;
 it1      =  1;
-KIT      =  0.01;
-dit      =  0;
+dit      =  0.01;
+KIT      =  1e-3;
 ct0      =  1;
 ct1      =  1;
-KCT      =  100;
-dct      =  0;
-si0      =  1;
-si1      =  1;
-dsi      =  0;
+dct      =  -0.01;
+KCT      =  1e3;
+si0      =  0;
+si1      =  0;
+dsi      =  1;
 
 % set model rheology parameters
 etam     =  1e3;                 % melt viscosity [Pas]
@@ -101,15 +101,15 @@ g0       =  10.;                 % gravity [m/s2]
 
 % set numerical model parameters
 CFL      =  0.50;                % (physical) time stepping courant number (multiplies stable step) [0,1]
-ADVN     =  'FRM';               % advection scheme ('UPW2', 'UPW3', or 'FRM')
+ADVN     =  'FLXDIV';              % advection scheme ('UPW2', 'UPW3', or 'FRM')
 theta    =  0.50;                % time-stepping scheme selector (1=BE, 1/2=CN, 0=FE)
 rtol     =  1e-3;                % outer its relative tolerance
 atol     =  1e-6;                % outer its absolute tolerance
 maxit    =  20;                  % maximum outer its
 alpha    =  0.0;                 % iterative lag parameter phase equilibrium
-beta     =  0.75;                % iterative lag parameter reaction rates
+beta     =  0.5;                 % iterative lag parameter reaction rates
 gamma    =  0.0;                 % numerical compressibility [0,1]
-delta    =  10;                  % regularisation of settling speed
+delta    =  5;                   % regularisation of settling speed
 etactr   =  1e6;                 % minimum viscosity for regularisation
 TINY     =  1e-16;               % minimum cutoff phase, component fractions
 
