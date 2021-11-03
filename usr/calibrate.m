8b3a3b52789fc34acaf1a206fbbfa185d4ed0dc1
Tobias Keller <kellertobs@gmail.com>
2021-11-03 17:11:06 +0100

    updated calibration script to resolve simplified mineral assemblage and allow to hold figures and plot several curves in different line styles for comparison

--- usr/calibrate.m
+++ usr/calibrate.m
@@ -1,40 +1,50 @@
 % calibrate phase diagram
-clear;
-addpath('../src');
+clear; addpath('../src');
 
 % hold figures or plot new ones,choose line style
 holdfig   = 0;    % set to 1 to hold figures, to 0 for new figures
 linestyle = '-';
 
+if ~holdfig; close all; end
+    
 % set parameters
 cphs0    =  0.35;                % phase diagram lower bound composition [wt SiO2]
 cphs1    =  0.70;                % phase diagram upper bound composition [wt SiO2]
 Tphs0    =  750;                 % phase diagram lower bound temperature [degC]
 Tphs1    =  1750;                % phase diagram upper bound temperature [degC]
 PhDg     =  4.0;                 % Phase diagram curvature factor (> 1)
-perCm    =  0.55;                % peritectic liquidus composition [wt SiO2]
+perCm    =  0.53;                % peritectic liquidus composition [wt SiO2]
 perCx    =  0.50;                % peritectic solidus  composition [wt SiO2]
-perT     =  1050;                % peritectic temperature [degC]
+perT     =  1100;                % peritectic temperature [degC]
 clap     =  1e-7;                % Clapeyron slope for P-dependence of melting T [degC/Pa]
 dTH2O    =  1300;                % solidus shift from water content [degC/wt^0.75]
 
 % calculate T-dependence of melting model
-T = linspace(700,1400,1e4);    % temperature [degC]
-P = 1e8    .*ones(size(T));    % pressure [Pa]
-c = 0.48   .*ones(size(T));    % major component [wt SiO2]
+T = linspace(600,1400,1e3);    % temperature [degC]
+c = 0.52   .*ones(size(T));    % major component [wt SiO2]
 v = 0.01   .*ones(size(T));    % volatile component [wt H2O]
-
-xq = ones(size(T)).*0.5;
-fq = ones(size(T)).*0.0;
+P = 1e8    .*ones(size(T));    % pressure [Pa]
 
 % equilibrium phase fractions and compositions
-[xq,cxq,cmq,fq,vfq,vmq]  =  equilibrium(xq,fq,T,c,v,P,Tphs0,Tphs1,cphs0,cphs1,perT,perCx,perCm,clap,dTH2O,PhDg);
+[xq,cxq,cmq,fq,vfq,vmq]  =  equilibrium(ones(size(T)).*0.5,ones(size(T)).*0.0, ...
+                                        T, c, v, P, ...
+                                        Tphs0,Tphs1,cphs0,cphs1,perT,perCx,perCm,clap,dTH2O,PhDg);
 mq = 1-fq-xq;  
 
-% simplified mineral assemblage
-ol = max(0,min(1,(cxq-perCx)./(cphs0-perCx))).*(cxq< perCx);
-px = max(0,min(1,(cxq-cphs0)./(perCx-cphs0))).*(cxq< perCx) + max(0,min(1,(cxq-cphs1)./(perCx-cphs1))).*(cxq>=perCx);
-fs = max(0,min(1,(cxq-perCx)./(cphs1-perCx))).*(cxq>=perCx);
+% simplified mineral assemblage (ol+px+fs+qz)
+ol0 = perCx; ol1 = cphs0; 
+px0 = (cphs0+perCx)/2; px1 = perCx;  px2 = cphs1;
+fs0 = perCx; fs1 = cphs1;  fs2 = (1+cphs1)/2;
+qz0 = perCm; qz1 = 1;
+
+ol = max(0,min(1,(cxq-ol0)./(ol1-ol0))).*(cxq>=ol1 & cxq<=ol0);
+px = max(0,min(1,(cxq-px0)./(px1-px0))).*(cxq>=px0 & cxq<=px1) + max(0,min(1,(cxq-px2)./(px1-px2))).*(cxq>=px1 & cxq<=px2);
+fs = max(0,min(1,(cxq-fs0)./(fs1-fs0))).*(cxq>=fs0 & cxq<=fs1) + max(0,min(1,(cxq-fs2)./(fs1-fs2))).*(cxq>=fs1 & cxq<=fs2);
+qz = max(0,min(1,(cxq-qz0)./(qz1-qz0))).*(cxq>=qz0 & cxq<=qz1);
+ol = ol./(ol+px+fs+qz);
+px = px./(ol+px+fs+qz);
+fs = fs./(ol+px+fs+qz);
+qz = qz./(ol+px+fs+qz);
 
 % block out values below solidus and above liquidus
 cxq(xq<1e-12 | mq<1e-12) = nan;
@@ -44,12 +54,13 @@ vmq(xq<1e-12 | mq<1e-12) = nan;
 ol (xq<1e-12 | mq<1e-12) = nan;
 px (xq<1e-12 | mq<1e-12) = nan;
 fs (xq<1e-12 | mq<1e-12) = nan;
+qz (xq<1e-12 | mq<1e-12) = nan;
 fq (xq<1e-12 | mq<1e-12) = nan;
 xq (xq<1e-12 | mq<1e-12) = nan;
 mq (mq>1-1e-12 | mq<1e-12) = nan;
 
 % plot phase diagram
-figure(1); if ~holdfig; clf; end
+figure(1);
 TT = linspace(Tphs0,Tphs1,1e3);
 cc = [linspace(cphs1,(perCx+perCm)/2,(perT-Tphs0)./(Tphs1-Tphs0)*1e3),linspace((perCx+perCm)/2,cphs0,(perT-Tphs1)./(Tphs0-Tphs1)*1e3)];
 [~,CCx,CCm,FF,~,~] = equilibrium(0*TT,0*TT,TT,cc,0*TT,0*TT,Tphs0,Tphs1,cphs0,cphs1,perT,perCx,perCm,clap,dTH2O,PhDg);
@@ -61,36 +72,36 @@ xlabel('Major component [wt SiO$_2$]','Interpreter','latex','FontSize',18)
 ylabel('Temperature [$^\circ$C]','Interpreter','latex','FontSize',18)
 
 % plot phase fractions
-figure(2); if ~holdfig; clf; end
-plot(T,xq,'k',T,mq,'r',T,fq,'b','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
-legend('crystals','melt','fluid','Interpreter','latex','FontSize',18,'box','off','location','east')
+figure(2);
+plot(T,xq,'k',T,mq,'r',T,fq.*10,'b','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
+legend('crystals','melt','fluid $\times10$','Interpreter','latex','FontSize',18,'box','off','location','east')
 set(gca,'TickLabelInterpreter','latex','FontSize',15)
 title('Melting model','Interpreter','latex','FontSize',22)
 xlabel('Temperature [$^\circ$C]','Interpreter','latex','FontSize',18)
 ylabel('Phase fractions [wt]','Interpreter','latex','FontSize',18)
 
 % plot major phase compositions
-figure(3); if ~holdfig; clf; end
+figure(3);
 plot(T,cxq,'b',T,cmq,'r','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
-legend('crystals','melt','Interpreter','latex','FontSize',18,'box','off','location','east')
+legend('crystals','melt','Interpreter','latex','FontSize',18,'box','off','location','northeast')
 set(gca,'TickLabelInterpreter','latex','FontSize',15)
 title('Phase compositions','Interpreter','latex','FontSize',22)
 xlabel('Temperature [$^\circ$C]','Interpreter','latex','FontSize',18)
 ylabel('Major component [wt SiO$_2$]','Interpreter','latex','FontSize',18)
 
 % plot volatile phase compositions
-figure(4); if ~holdfig; clf; end
-plot(T,vmq,'r','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
-legend('melt','Interpreter','latex','FontSize',18,'box','off','location','east')
+figure(4);
+plot(T,vfq/10,'b',T,vmq,'r','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
+legend('fluid $/10$','melt','Interpreter','latex','FontSize',18,'box','off','location','northeast')
 set(gca,'TickLabelInterpreter','latex','FontSize',15)
 title('Phase compositions','Interpreter','latex','FontSize',22)
 xlabel('Temperature [$^\circ$C]','Interpreter','latex','FontSize',18)
 ylabel('Volatile component [wt H$_2$O]','Interpreter','latex','FontSize',18)
 
 % plot simplified mineral assemblage
-figure(5); if ~holdfig; clf; end
-plot(T,ol,'k',T,px,'r',T,fs,'b','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
-legend('olivine','pyroxene','feldspar','Interpreter','latex','FontSize',18,'box','off','location','east')
+figure(5);
+plot(T,ol,'g',T,px,'k',T,fs,'b',T,qz,'r','LineStyle',linestyle,'LineWidth',2); hold on; box on; axis tight;
+legend('olivine','pyroxene','feldspar','quartz','Interpreter','latex','FontSize',18,'box','off','location','west')
 set(gca,'TickLabelInterpreter','latex','FontSize',15)
 title('Mineral assemblage','Interpreter','latex','FontSize',22)
 xlabel('Temperature [$^\circ$C]','Interpreter','latex','FontSize',18)
