1c94c6a9225e118b40e4b6ecfd63c7bd37b5ddc6
Tobias Keller <Tobias.Keller@glasgow.ac.uk>
2021-11-26 07:29:30 +0100

    clean out old code no longer needed

--- src/update.m
+++ src/update.m
@@ -3,7 +3,7 @@
 % update phase densities
 rhom = rhom0 .* (1 - aTm.*(T-Tphs0) - gCm.*(cm-cphs0));
 rhox = rhox0 .* (1 - aTx.*(T-Tphs0) - gCx.*(cx-cphs0));
-rhof = rhof0 .* (1 - aTf.*(T-Tphs0) + bPf.*(Pt-Ptop));
+rhof = rhof0 .* (1 - aTf.*(T-Tphs0) + bPf.*(Pt-Ptop ));
 
 % convert weight to volume fraction, update bulk density
 res = 1;    tol = 1e-15;
@@ -15,8 +15,6 @@ while res > tol
     rho  = mu.*rhom + chi.*rhox + phi.*rhof;
     res  = (norm(chi(:)-chii(:),2) + norm(phi(:)-phii(:),2))./sqrt(2*length(chi(:)));
 end
-% rhoBF  = ((rho(2:end-2,2:end-1)+rhoo(2:end-2,2:end-1))/2 ...
-%          +(rho(3:end-1,2:end-1)+rhoo(3:end-1,2:end-1))/2)/2 - rhoref;      % relative density for bouancy force term
 rhoBF  = (rho(2:end-2,2:end-1)+rho(3:end-1,2:end-1))/2 - rhoref;           % relative density for bouancy force term
 Pt     = Ptop + rhoref.*g0.*ZZ + P;                                        % total pressure
 
@@ -28,13 +26,9 @@ kT     = mu.*kTm + chi.*kTx + phi.*kTf;                                    % mag
 etam  = etam0 .* exp(Em./(8.3145.*(T+273.15))-Em./(8.3145.*((Tphs0+Tphs1)/2+273.15))) ...
               .* Fmc.^(cm-(cphs0+cphs1)/2) .* Fmv.^(vm./0.01) ...
               .* max(1e-12,1-min(1-TINY,phi/phic)).^-A .* max(1e-12,1-min(1-TINY,chi/chic)).^-B; % T-c-v-chi-phi-dep. melt viscosity
-% etax  = etax0 .* exp(Ex./(8.3145.*(T+273.15))-Ex./(8.3145.*(Tphs0+273.15))) ...
-%               .* Fxc.^(cx-(cphs0+cphs1)/2) ...
-%               .* exp(-lambda.*min(chic,1-chi));                            % T-c-chi-dep. crystal viscosity
 etaf  = etaf0.* ones(size(f));                                             % constant volatile fluid viscosity
 eta   = 1./(1./(etam + etaf) + 1./etax0);                                   % effective magma viscosity
 eta   = 1./(1./(eta + etamin) + 1./etamax);                                % limit viscosity range
-% if step>0; eta   = (etao + eta)/2; end
 
 etac  = (eta(1:end-1,1:end-1)+eta(2:end,1:end-1) ...                       % viscosity in cell corners
       +  eta(1:end-1,2:end  )+eta(2:end,2:end  ))./4;
@@ -74,33 +68,18 @@ tII([1 end],:) = tII([2 end-1],:);
 if bndmode==3; sds = -1;      % no slip
 else;          sds = +1; end  % free slip
 
-wf = 2/9   .* ((rhof(1:end-1,:)+rhof(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*df^2./((eta(1:end-1,:)+eta(2:end,:))/2) ...  % bubble flotation speed
-   + 1/500 .* ((rhof(1:end-1,:)+rhof(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2.*((phi(1:end-1,:)+phi(2:end,:))/2).^2./((etaf(1:end-1,:)+etaf(2:end,:))/2); % fluid percolation speed
-wf([1 end],:) = 0;
-wf(:,[1 end]) = sds*wf(:,[2 end-1]);
-for d = 1:delta
-    wf(2:end-1,2:end-1) = wf(2:end-1,2:end-1) + diff(wf(:,2:end-1),2,1)./8 + diff(wf(2:end-1,:),2,2)./8;
-    wf([1 end],:) = 0;
-    wf(:,[1 end]) = sds*wf(:,[2 end-1]);
-end
-
-wx = 2/9 .* ((rhox(1:end-1,:)+rhox(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2./((eta(1:end-1,:)+eta(2:end,:))/2); % crystal settling speed
+wx = 2/9   .* ((rhox(1:end-1,:)+rhox(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2./((eta(1:end-1,:)+eta(2:end,:))/2); % crystal settling speed
 wx([1 end],:) = 0;
 wx(:,[1 end]) = sds*wx(:,[2 end-1]);
-for d = 1:delta
-    wx(2:end-1,2:end-1) = wx(2:end-1,2:end-1) + diff(wx(:,2:end-1),2,1)./8 + diff(wx(2:end-1,:),2,2)./8;
-    wx([1 end],:) = 0;
-    wx(:,[1 end]) = sds*wx(:,[2 end-1]);
-end
 
-wm = 1/50 .* ((rhom(1:end-1,:)+rhom(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2.*((mu(1:end-1,:)+mu(2:end,:))/2).^2./((etam(1:end-1,:)+etam(2:end,:))/2); % melt percolation speed
+wf = 2/9   .* ((rhof(1:end-1,:)+rhof(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*df^2./((eta(1:end-1,:)+eta(2:end,:))/2) ...  % bubble flotation speed
+   + 1/500 .* ((rhof(1:end-1,:)+rhof(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2.*((phi(1:end-1,:)+phi(2:end,:))/2).^2.*((chi(1:end-1,:)+chi(2:end,:))/2).^2./((etaf(1:end-1,:)+etaf(2:end,:))/2); % fluid percolation speed
+wf([1 end],:) = 0;
+wf(:,[1 end]) = sds*wf(:,[2 end-1]);
+
+wm = 1/50  .* ((rhom(1:end-1,:)+rhom(2:end,:))/2-(rho(1:end-1,:)+rho(2:end,:))/2)*g0*dx^2.*((mu (1:end-1,:)+mu (2:end,:))/2).^2.*((chi(1:end-1,:)+chi(2:end,:))/2).^2./((etam(1:end-1,:)+etam(2:end,:))/2); % melt percolation speed
 wm([1 end],:) = 0;
 wm(:,[1 end]) = sds*wm(:,[2 end-1]);
-for d = 1:delta
-    wm(2:end-1,2:end-1) = wm(2:end-1,2:end-1) + diff(wm(:,2:end-1),2,1)./8 + diff(wm(2:end-1,:),2,2)./8;
-    wm([1 end],:) = 0;
-    wm(:,[1 end]) = sds*wm(:,[2 end-1]);
-end
 
 % update phase velocities
 Wf   = W + wf;                                                             % mvp z-velocity
