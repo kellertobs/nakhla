7f087f9a0cf9dfd3b3d5348e06c4dfc925884e02
Tobias Keller <kellertobs@gmail.com>
2022-01-19 16:50:24 +0100

    minor: rename some variables to avoid double usage

--- src/fluidmech.m
+++ src/fluidmech.m
@@ -1,15 +1,16 @@
 %% assemble coefficients for matrix velocity diagonal and right-hand side
-II  = [];       % equation indeces into A
-JJ  = [];       % variable indeces into A
-AA  = [];       % coefficients for A
-IR  = [];       % equation indeces into R
-RR  = [];       % forcing entries for R
+IIL  = [];       % equation indeces into L
+JJL  = [];       % variable indeces into L
+AAL  = [];       % coefficients for L
+IIR  = [];       % equation indeces into R
+AAR  = [];       % forcing entries for R
 
 
 % set cooling boundaries to no slip, else to free slip
-if bndmode==4; sds = +1;      % no slip
+if bndmode>=4; sds = +1;      % no slip
 else;          sds = -1; end  % free slip
-if bndmode >0; top = +1;      % no slip
+if bndmode==5; top = -1;      % free slip
+elseif bndmode >0; top = +1;  % no slip
 else;          top = -1; end  % free slip
 if bndmode >1; bot = +1;      % no slip
 else;          bot = -1; end  % free slip
@@ -20,30 +21,30 @@ else;          bot = -1; end  % free slip
 % left boundary
 ii = MapW(:,1); jj1 = ii; jj2 = MapW(:,2);
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+sds];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+sds];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % right boundary
 ii = MapW(:,end); jj1 = ii; jj2 = MapW(:,end-1);
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+sds];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+sds];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % top boundary
 ii = MapW(1,2:end-1); jj = ii;
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj(:)];   AA = [AA; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj(:)];   AAL = [AAL; aa(:)+1];
 aa = zeros(size(ii)) - WBG(1,2:end-1);
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % bottom boundary
 ii = MapW(end,2:end-1); jj = ii;
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj(:)];   AA = [AA; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj(:)];   AAL = [AAL; aa(:)+1];
 aa = zeros(size(ii)) - WBG(end,2:end-1);
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 
 % internal points
@@ -56,27 +57,27 @@ EtaP1 = eta (2:end-2,2:end-1); EtaP2 = eta (3:end-1,2:end-1);
 jj1 = MapW(1:end-2,2:end-1); jj2 = MapW(3:end,2:end-1); jj3 = MapW(2:end-1,1:end-2); jj4 = MapW(2:end-1,3:end);
 
 aa = - 2/3*(EtaP1+EtaP2)/h^2 - 1/2*(EtaC1+EtaC2)/h^2;
-II = [II; ii(:)]; JJ = [JJ;  ii(:)];   AA = [AA; aa(:)           ];      % W on stencil centre
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; 2/3*EtaP1(:)/h^2];      % W one above
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; 2/3*EtaP2(:)/h^2];      % W one below
-II = [II; ii(:)]; JJ = [JJ; jj3(:)];   AA = [AA; 1/2*EtaC1(:)/h^2];      % W one to the left
-II = [II; ii(:)]; JJ = [JJ; jj4(:)];   AA = [AA; 1/2*EtaC2(:)/h^2];      % W one to the right
+IIL = [IIL; ii(:)]; JJL = [JJL;  ii(:)];   AAL = [AAL; aa(:)           ];      % W on stencil centre
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; 2/3*EtaP1(:)/h^2];      % W one above
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; 2/3*EtaP2(:)/h^2];      % W one below
+IIL = [IIL; ii(:)]; JJL = [JJL; jj3(:)];   AAL = [AAL; 1/2*EtaC1(:)/h^2];      % W one to the left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj4(:)];   AAL = [AAL; 1/2*EtaC2(:)/h^2];      % W one to the right
 
 % coefficients multiplying x-velocities U
 %         top left         ||        bottom left          ||       top right       ||       bottom right
 jj1 = MapU(2:end-2,1:end-1); jj2 = MapU(3:end-1,1:end-1); jj3 = MapU(2:end-2,2:end); jj4 = MapU(3:end-1,2:end);
 
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; (1/2*EtaC1(:)-1/3*EtaP1(:))/h^2];  % W one to the top and left
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA;-(1/2*EtaC1(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and left
-II = [II; ii(:)]; JJ = [JJ; jj3(:)];   AA = [AA;-(1/2*EtaC2(:)-1/3*EtaP1(:))/h^2];  % W one to the top and right
-II = [II; ii(:)]; JJ = [JJ; jj4(:)];   AA = [AA; (1/2*EtaC2(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; (1/2*EtaC1(:)-1/3*EtaP1(:))/h^2];  % W one to the top and left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL;-(1/2*EtaC1(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj3(:)];   AAL = [AAL;-(1/2*EtaC2(:)-1/3*EtaP1(:))/h^2];  % W one to the top and right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj4(:)];   AAL = [AAL; (1/2*EtaC2(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and right
 
 
 % z-RHS vector
 
 rr = rhoBF .* g0;
 
-IR = [IR; ii(:)];  RR = [RR; rr(:)];
+IIR = [IIR; ii(:)];  AAR = [AAR; rr(:)];
 
 
 %  assemble coefficients of x-stress divergence
@@ -84,30 +85,30 @@ IR = [IR; ii(:)];  RR = [RR; rr(:)];
 % top boundary
 ii = MapU(1,:); jj1 = ii; jj2 = MapU(2,:);
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+top];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+top];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % bottom boundary
 ii = MapU(end,:); jj1 = ii; jj2 = MapU(end-1,:);
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+bot];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+bot];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % left side boundary
 ii = MapU(2:end-1,1); jj = ii;
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj(:)];   AA = [AA; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj(:)];   AAL = [AAL; aa(:)+1];
 aa = zeros(size(ii)) - UBG(2:end-1,1);
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 % right side boundary
 ii = MapU(2:end-1,end); jj = ii;
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj(:)];   AA = [AA; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj(:)];   AAL = [AAL; aa(:)+1];
 aa = zeros(size(ii)) - UBG(2:end-1,end);
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 
 % internal points
@@ -120,36 +121,36 @@ EtaP1 = eta (2:end-1,2:end-2); EtaP2 = eta (2:end-1,3:end-1);
 jj1 = MapU(2:end-1,1:end-2); jj2 = MapU(2:end-1,3:end); jj3 = MapU(1:end-2,2:end-1); jj4 = MapU(3:end,2:end-1);
 
 aa = - 2/3*(EtaP1+EtaP2)/h^2 - 1/2*(EtaC1+EtaC2)/h^2;
-II = [II; ii(:)]; JJ = [JJ;  ii(:)];   AA = [AA; aa(:)           ];      % U on stencil centre
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; 2/3*EtaP1(:)/h^2];      % U one to the left
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; 2/3*EtaP2(:)/h^2];      % U one to the right
-II = [II; ii(:)]; JJ = [JJ; jj3(:)];   AA = [AA; 1/2*EtaC1(:)/h^2];      % U one above
-II = [II; ii(:)]; JJ = [JJ; jj4(:)];   AA = [AA; 1/2*EtaC2(:)/h^2];      % U one below
+IIL = [IIL; ii(:)]; JJL = [JJL;  ii(:)];   AAL = [AAL; aa(:)           ];      % U on stencil centre
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; 2/3*EtaP1(:)/h^2];      % U one to the left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; 2/3*EtaP2(:)/h^2];      % U one to the right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj3(:)];   AAL = [AAL; 1/2*EtaC1(:)/h^2];      % U one above
+IIL = [IIL; ii(:)]; JJL = [JJL; jj4(:)];   AAL = [AAL; 1/2*EtaC2(:)/h^2];      % U one below
 
 % coefficients multiplying z-velocities W
 %         top left         ||        top right          ||       bottom left       ||       bottom right
 jj1 = MapW(1:end-1,2:end-2); jj2 = MapW(1:end-1,3:end-1); jj3 = MapW(2:end,2:end-2); jj4 = MapW(2:end,3:end-1);
 
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; (1/2*EtaC1(:)-1/3*EtaP1(:))/h^2];  % W one to the top and left
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA;-(1/2*EtaC1(:)-1/3*EtaP2(:))/h^2];  % W one to the top and right
-II = [II; ii(:)]; JJ = [JJ; jj3(:)];   AA = [AA;-(1/2*EtaC2(:)-1/3*EtaP1(:))/h^2];  % W one to the bottom and left
-II = [II; ii(:)]; JJ = [JJ; jj4(:)];   AA = [AA; (1/2*EtaC2(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; (1/2*EtaC1(:)-1/3*EtaP1(:))/h^2];  % W one to the top and left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL;-(1/2*EtaC1(:)-1/3*EtaP2(:))/h^2];  % W one to the top and right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj3(:)];   AAL = [AAL;-(1/2*EtaC2(:)-1/3*EtaP1(:))/h^2];  % W one to the bottom and left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj4(:)];   AAL = [AAL; (1/2*EtaC2(:)-1/3*EtaP2(:))/h^2];  % W one to the bottom and right
 
 
 % x-RHS vector
 rr = zeros(size(ii));
-IR = [IR; ii(:)];  RR = [RR; rr(:)];
+IIR = [IIR; ii(:)];  AAR = [AAR; rr(:)];
 
 
 % assemble coefficient matrix & right-hand side vector
-KV = sparse(II,JJ,AA,NW+NU,NW+NU);
-RV = sparse(IR,ones(size(IR)),RR);
+KV = sparse(IIL,JJL,AAL,NW+NU,NW+NU);
+RV = sparse(IIR,ones(size(IIR)),AAR);
 
 
 %% assemble coefficients for gradient operator
-II  = [];       % equation indeces into A
-JJ  = [];       % variable indeces into A
-AA  = [];       % coefficients for A
+IIL  = [];       % equation indeces into A
+JJL  = [];       % variable indeces into A
+AAL  = [];       % coefficients for A
 
 
 % coefficients for z-gradient
@@ -159,8 +160,8 @@ ii = MapW(2:end-1,2:end-1);
 jj1 = MapP(2:end-2,2:end-1); jj2 = MapP(3:end-1,2:end-1);
 
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)-1/h];     % one to the top
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+1/h];     % one to the bottom
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)-1/h];     % one to the top
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+1/h];     % one to the bottom
 
 
 % coefficients for x-gradient
@@ -170,18 +171,18 @@ ii = MapU(2:end-1,2:end-1);
 jj1 = MapP(2:end-1,2:end-2); jj2 = MapP(2:end-1,3:end-1);
 
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)-1/h];     % one to the left
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+1/h];     % one to the right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)-1/h];     % one to the left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+1/h];     % one to the right
 
 
 % assemble coefficient matrix
-GG = sparse(II,JJ,AA,NW+NU,NP);
+GG = sparse(IIL,JJL,AAL,NW+NU,NP);
 
 
 %% assemble coefficients for divergence operator
-II  = [];       % equation indeces into A
-JJ  = [];       % variable indeces into A
-AA  = [];       % coefficients for A
+IIL  = [];       % equation indeces into A
+JJL  = [];       % variable indeces into A
+AAL  = [];       % coefficients for A
 
 %internal points
 ii = MapP(2:end-1,2:end-1);
@@ -191,21 +192,21 @@ ii = MapP(2:end-1,2:end-1);
 jj1 = MapU(2:end-1,1:end-1); jj2 = MapU(2:end-1,2:end); jj3 = MapW(1:end-1,2:end-1); jj4 = MapW(2:end,2:end-1);
 
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)-1/h];  % U one to the left
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)+1/h];  % U one to the right
-II = [II; ii(:)]; JJ = [JJ; jj3(:)];   AA = [AA; aa(:)-1/h];  % W one above
-II = [II; ii(:)]; JJ = [JJ; jj4(:)];   AA = [AA; aa(:)+1/h];  % W one below
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)-1/h];  % U one to the left
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)+1/h];  % U one to the right
+IIL = [IIL; ii(:)]; JJL = [JJL; jj3(:)];   AAL = [AAL; aa(:)-1/h];  % W one above
+IIL = [IIL; ii(:)]; JJL = [JJL; jj4(:)];   AAL = [AAL; aa(:)+1/h];  % W one below
 
 % assemble coefficient matrix
-DD = sparse(II,JJ,AA,NP,NW+NU);
+DD = sparse(IIL,JJL,AAL,NP,NW+NU);
 
 
 %% assemble coefficients for matrix pressure diagonal and right-hand side
-II  = [];       % equation indeces into A
-JJ  = [];       % variable indeces into A
-AA  = [];       % coefficients for A
-IR  = [];       % equation indeces into R
-RR  = [];       % forcing entries for R
+IIL  = [];       % equation indeces into A
+JJL  = [];       % variable indeces into A
+AAL  = [];       % coefficients for A
+IIR  = [];       % equation indeces into R
+AAR  = [];       % forcing entries for R
 
 
 % boundary points
@@ -214,18 +215,18 @@ jj1 = ii;
 jj2 = [MapP(2,:).'; MapP(end-1,:).'];
 
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)-1];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)-1];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 ii  = [MapP(:,1); MapP(:,end  )]; % left & right
 jj1 = ii;
 jj2 = [MapP(:,2); MapP(:,end-1)];
 
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; jj1(:)];   AA = [AA; aa(:)+1];
-II = [II; ii(:)]; JJ = [JJ; jj2(:)];   AA = [AA; aa(:)-1];
-IR = [IR; ii(:)]; RR = [RR; aa(:)];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj1(:)];   AAL = [AAL; aa(:)+1];
+IIL = [IIL; ii(:)]; JJL = [JJL; jj2(:)];   AAL = [AAL; aa(:)-1];
+IIR = [IIR; ii(:)]; AAR = [AAR; aa(:)];
 
 
 % internal points
@@ -233,20 +234,20 @@ ii = MapP(2:end-1,2:end-1);
 
 % coefficients multiplying matrix pressure P
 aa = zeros(size(ii));
-II = [II; ii(:)]; JJ = [JJ; ii(:)];    AA = [AA; aa(:)];  % P on stencil centre
+IIL = [IIL; ii(:)]; JJL = [JJL; ii(:)];    AAL = [AAL; aa(:)];  % P on stencil centre
 
 
 % RHS
 rr = VolSrc(2:end-1,2:end-1);
-IR = [IR; ii(:)];
-RR = [RR; rr(:)];
+IIR = [IIR; ii(:)];
+AAR = [AAR; rr(:)];
 
 
 % assemble coefficient matrix and right-hand side vector
-KP = sparse(II,JJ,AA,NP,NP);
-RP = sparse(IR,ones(size(IR)),RR,NP,1);
+KP = sparse(IIL,JJL,AAL,NP,NP);
+RP = sparse(IIR,ones(size(IIR)),AAR,NP,1);
 
-np = round(N-2)/2+1;
+np = round((N-2)/2)+1;
 KP(MapP(np,np),:) = 0;
 KP(MapP(np,np),MapP(np,np)) = 1;
 RP(MapP(np,np),:) = 0;
