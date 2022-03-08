2bc3ec64fdbb4f21e6512dc26a1e321649ebdb6d
Tobias Keller <Tobias.Keller@glasgow.ac.uk>
2022-03-08 21:08:09 +0100

    avoid nan on volatile error in anhydrous runs

--- src/history.m
+++ src/history.m
@@ -28,10 +28,10 @@ if step>0; hist.DC(step+1) = hist.DC(step) + dsumCdt.*dt; else; hist.DC(step+1) 
 if step>0; hist.DV(step+1) = hist.DV(step) + dsumVdt.*dt; else; hist.DV(step+1) = 0; end  % [kg]
 
 % record conservation error of mass M, heat H, major component C, volatile component V
-hist.EM(step+1) = (hist.sumM(step+1) - hist.DM(step+1))./hist.sumM(1) - 1;  % [kg/kg]
-hist.EH(step+1) = (hist.sumH(step+1) - hist.DH(step+1))./hist.sumH(1) - 1;  % [J /J ]
-hist.EC(step+1) = (hist.sumC(step+1) - hist.DC(step+1))./hist.sumC(1) - 1;  % [kg/kg]
-hist.EV(step+1) = (hist.sumV(step+1) - hist.DV(step+1))./hist.sumV(1) - 1;  % [kg/kg]
+hist.EM(step+1) = (hist.sumM(step+1) - hist.DM(step+1))./(hist.sumM(1)+TINY) - 1;  % [kg/kg]
+hist.EH(step+1) = (hist.sumH(step+1) - hist.DH(step+1))./(hist.sumH(1)+TINY) - 1;  % [J /J ]
+hist.EC(step+1) = (hist.sumC(step+1) - hist.DC(step+1))./(hist.sumC(1)+TINY) - 1;  % [kg/kg]
+hist.EV(step+1) = (hist.sumV(step+1) - hist.DV(step+1))./(hist.sumV(1)+TINY) - 1;  % [kg/kg]
 
 % record variable and coefficient diagnostics
 hist.W(step+1,1) = min(min(-W(:,2:end-1)));
