2ae3d9b1d9ce831ccaa553b6899f23ee6e0302a9
Tobias Keller <kellertobs@gmail.com>
2022-05-13 16:41:56 +0200

    updated standard run scripts for fractional crystallisation

--- usr/run_0D_fract_anh.m
+++ usr/run_0D_fract_anh.m
@@ -31,9 +31,9 @@ seed     =  15;                  % random perturbation seed
 smth     =  (N/30)^2;            % regularisation of initial random perturbation
 zlay     =  0.5;                 % layer thickness (relative to domain depth D)
 wlay_T   =  1e-6;                % thickness of smooth layer boundary (relative to domain depth D)
-wlay_c   =  2*h/D;               % thickness of smooth layer boundary (relative to domain depth D)
-T0       =  1400;                % temperature top layer [deg C]
-T1       =  1400;                % temperature base layer [deg C]
+wlay_c   =  3*h/D;               % thickness of smooth layer boundary (relative to domain depth D)
+T0       =  1375;                % temperature top layer [deg C]
+T1       =  1375;                % temperature base layer [deg C]
 dT       =  0;                   % amplitude of random noise [deg C]
 c0       =  0.50;                % major component top layer [wt SiO2]
 c1       =  0.50;                % major component base layer [wt SiO2]
@@ -59,8 +59,8 @@ ri1      =  1;                   % radiogenic isotope base layer [wt ppm]
 dri      =  -0.00;               % radiogenic isotope random noise [wt ppm]
 KRIP     =  10;                  % radiogenic parent isotope partition coefficient
 KRID     =  0.1;                 % radiogenic daughter isotope partition coefficient
-HLRIP    =  1e4*yr;              % radiogenic parent isotope half-life [s]
-HLRID    =  1e3*yr;              % radiogenic daughter isotope half-life [s]
+HLRIP    =  1e3*yr;              % radiogenic parent isotope half-life [s]
+HLRID    =  1e2*yr;              % radiogenic daughter isotope half-life [s]
 
 % set thermo-chemical boundary parameters
 Ptop     =  1e8;                 % top pressure [Pa]
@@ -69,8 +69,8 @@ bndinit  =  0;                   % switch on (1) to initialise with already esta
 dw       =  1*h;                 % boundary layer thickness for assimilation [m]
 fin      =  0;                   % ingassing factor (0 = no ingassing; 1 = free flow ingassing)
 fout     =  0;                   % outgassing factor (0 = no outgassing; 1 = free flow outgassing)
-tau_T    =  4*hr;                % wall cooling/assimilation time [s]
-tau_a    =  8*hr;                % wall cooling/assimilation time [s]
+tau_T    =  2*hr;                % wall cooling/assimilation time [s]
+tau_a    =  2*hr;                % wall cooling/assimilation time [s]
 Twall    =  500;                 % wall temperature [degC] (nan = insulating)
 cwall    =  nan;                 % wall major component [wt SiO2] (nan = no assimilation)
 vwall    =  nan;                 % wall volatile component [wt H2O] (nan = no assimilation)
@@ -80,15 +80,16 @@ siwall   =  nan;                 % wall stable isotope [delta] (nan = no assimil
 riwall   =  nan;                 % wall radiogenic isotope [wt ppm] (nan = no assimilation)
 
 % set thermo-chemical material parameters
-kc       =  1e-7;                % chemical diffusivity [m^2/s]
-kTm      =  4;                   % melt thermal conductivity [W/m/k]
-kTx      =  1;                   % xtal thermal conductivity [W/m/k]
-kTf      =  0.02;                % mvp  thermal conductivity [W/m/k]
+kc       =  1e-2;                % chemical diffusivity [kg/m/s]
+kTm      =  4;                   % melt thermal conductivity [W/m/K]
+kTx      =  1;                   % xtal thermal conductivity [W/m/K]
+kTf      =  0.02;                % mvp  thermal conductivity [W/m/K]
 Cpm      =  1400;                % melt heat capacity [J/kg/K]
 Cpx      =  1000;                % xtal heat capacity [J/kg/K]
 Cpf      =  2000;                % mvp  heat capacity [J/kg/K]
-Dsx      = -300;                 % entropy change of crystallisation [J/kg/K]
-Dsf      =  400;                 % entropy change of exsolution [J/kg/K]
+Lx0      = -400e3;               % latent heat of crystallisation at cphs0 [J/kg]
+Lx1      = -300e3;               % latent heat of crystallisation at cphs1 [J/kg]
+Lf       =  500e3;               % latent heat of exsolution [J/kg]
 
 % set phase diagram parameters
 cphs0    =  0.43;                % phase diagram lower bound composition [wt SiO2]
@@ -101,13 +102,13 @@ perCx    =  0.475;               % peritectic solidus  composition [wt SiO2]
 perT     =  1147;                % peritectic temperature [degC]
 clap     =  1e-7;                % Clapeyron slope for P-dependence of melting T [degC/Pa]
 dTH2O    =  [1200,1000,100];     % solidus shift from water content [degC/wt^0.75]
-tau_r    =  10;                  % reaction time [s]
+tau_r    =  60;                  % reaction time [s]
 
 % set model rheology parameters
 etam0    =  300;                 % melt viscosity [Pas]
 etaf0    =  0.1;                 % fluid viscosity [Pas]
 etax0    =  1e15;                % crystal viscosity [Pas]
-Fmc      =  5e+5;                % major component weakening factor of melt viscosity [1]
+Fmc      =  1e+5;                % major component weakening factor of melt viscosity [1]
 Fmv      =  0.6;                 % volatile component weakening factor of melt viscosity [1]
 Em       =  175e3;               % activation energy melt viscosity [J/mol]
 AA       = [ 0.60, 0.25, 0.30; 0.20, 0.20, 0.20; 0.20, 0.20, 0.20; ];  % permission slopes
@@ -126,17 +127,16 @@ gCx      =  0.5;                 % xtal compositional expansion [1/wt]
 bPf      =  1e-8;                % mvp compressibility [1/Pa]
 dx       =  1e-3;                % crystal size [m]
 df       =  1e-3;                % bubble size [m]
-dm       =  1e-4;                % melt film size [m]
 g0       =  10.;                 % gravity [m/s2]
 
 % set numerical model parameters
 CFL      =  0.5;                 % (physical) time stepping courant number (multiplies stable step) [0,1]
 ADVN     =  'FRM';               % advection scheme ('UPW2', 'UPW3', or 'FRM')
 theta    =  0.5;                 % time-stepping parameter (1 = 1st-order implicit; 1/2 = 2nd-order semi-implicit)
-rtol     =  1e-5;                % outer its relative tolerance
+rtol     =  1e-4;                % outer its relative tolerance
 atol     =  1e-7;                % outer its absolute tolerance
-maxit    =  100;                 % maximum outer its
-alpha    =  0.80;                % iterative lag parameter equilibration
+maxit    =  20;                  % maximum outer its
+alpha    =  0.75;                % iterative lag parameter equilibration
 beta     =  0.75;                % iterative lag parameter phase diagram
 delta    =  0;                   % smoothness of segregation speed
 etamin   =  1e1;                 % minimum viscosity for stabilisation
