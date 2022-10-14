69de11dae394eeedef8546be51d4c38ed90bf1ad
Tobias Keller <kellertobs@gmail.com>
2022-10-14 16:40:59 +0200

    slightly adjust calibration for desirable results

--- cal/cal_krafla.m
+++ cal/cal_krafla.m
@@ -6,13 +6,13 @@ cal.oxds   = [  42.7    0.0    0.0   57.3    0.0    0.0    0.0     % cphs0 => du
 cal.oxds = cal.oxds./sum(cal.oxds,2)*100;
 
 % specify phase diagram parameters
-cal.cphs0    =  0.425;               % phase diagram lower bound composition [wt SiO2]
-cal.cphs1    =  0.770;               % phase diagram upper bound composition [wt SiO2]
+cal.cphs0    =  0.42;                % phase diagram lower bound composition [wt SiO2]
+cal.cphs1    =  0.77;                % phase diagram upper bound composition [wt SiO2]
 cal.Tphs0    =  845;                 % phase diagram lower bound temperature [degC]
 cal.Tphs1    =  1870;                % phase diagram upper bound temperature [degC]
 cal.PhDg     =  [7.0,4.0,1.0,1.0];   % Phase diagram curvature factor (> 1)
-cal.perCm    =  0.515;               % peritectic liquidus composition [wt SiO2]
-cal.perCx    =  0.475;               % peritectic solidus  composition [wt SiO2]
+cal.perCm    =  0.517;               % peritectic liquidus composition [wt SiO2]
+cal.perCx    =  0.477;               % peritectic solidus  composition [wt SiO2]
 cal.perT     =  1145;                % peritectic temperature [degC]
 cal.clap     =  1e-7;                % Clapeyron slope for P-dependence of melting T [degC/Pa]
 cal.dTH2O    =  [1300,1100,900];     % solidus shift from water content [degC/wt^0.75]
