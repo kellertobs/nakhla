4d23aa5ffad48a143d1b6eb8e64d235d203f6d6d
Tobias Keller <kellertobs@gmail.com>
2022-11-01 16:49:52 +0100

    improve component/oxide calibration

--- cal/cal_krafla.m
+++ cal/cal_krafla.m
@@ -1,24 +1,24 @@
 % set melting model end-member oxide compositions
-cal.oxdStr =  {'SiO$_2$','Al$_2$O$_3$','FeO','MgO','CaO','Na$_2$O','K$_2$O'};
-cal.oxds   = [  42.7    0.0    0.0   57.3    0.0    0.0    0.0     % for
-                30.0    0.0   70.0    0.0    0.0    0.0    0.0     % fay
-                55.0    7.5    3.0   34.0    0.4    0.1    0.0     % opx
-                49.0    2.0   27.7    5.1   16.0    0.2    0.0     % cpx
-                44.4   35.8    0.0    0.0   19.2    0.6    0.0     % ant
-                67.3   20.2    0.0    0.0    0.8   11.2    0.5     % alb
-                64.8   18.3    0.0    0.0    0.0    0.0   16.9     % kfs
-               100.0    0.0    0.0    0.0    0.0    0.0    0.0];   % qtz
+cal.oxdStr =  {'SiO$_2$','TiO$_2$','Al$_2$O$_3$','FeO','MgO','CaO','Na$_2$O','K$_2$O'};
+cal.oxd    = [  42.7    0.0    0.0    0.0   57.3    0.0    0.0    0.0     % for
+                30.0    0.5    0.0   69.5    0.0    0.0    0.0    0.0     % fay
+                55.0    0.05   7.5    3.0   33.95   0.4    0.1    0.0     % opx
+                49.0    0.1    2.0   27.6    5.1   16.0    0.2    0.0     % cpx
+                44.4    0.0   35.8    0.0    0.0   19.2    0.6    0.0     % ant
+                67.3    0.0   20.2    0.0    0.0    0.8   11.2    0.5     % alb
+                64.8    0.0   18.3    0.0    0.0    0.0    0.0   16.9     % kfs
+                99.99   0.01   0.0    0.0    0.0    0.0    0.0    0.0];   % qtz
 
 cal.cmpStr = {'for','fay','opx','cpx','ant','alb','kfs','qtz'};
-cal.cmps   = [ 100.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0     % cphs0 => dunite
-                20.0   11.0   34.0   35.0    0.0    0.0    0.0    0.0     % perCx => pyroxenite
-                 2.0    2.0   18.0   30.0   28.0   20.0    0.0    0.0     % perCm => basalt
+cal.cmp    = [ 100.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0     % cphs0 => dunite
+                12.0    8.0   38.0   18.0   24.0    0.0    0.0    0.0     % perCx => pyroxenite
+                 1.0    1.5   15.0   34.0   28.0   20.0    0.5    0.0     % perCm => basalt
                  0.0    0.0    0.0    8.0    9.0   15.0   31.0   37.0];   % cphs1 => rhyolite
 
-cal.oxds = cal.oxds./sum(cal.oxds,2)*100;
-cal.cmps = cal.cmps./sum(cal.cmps,2)*100;
+cal.oxd = cal.oxd./sum(cal.oxd,2)*100;
+cal.cmp = cal.cmp./sum(cal.cmp,2)*100;
 
-p = cal.cmps*cal.oxds./100;
+p = cal.cmp*cal.oxd./100;
 
 % specify phase diagram parameters
 cal.cphs0    =  p(1,1)/100;          % phase diagram lower bound composition [wt SiO2]
