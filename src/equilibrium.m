a44a5fcaedc46852e5181b6cc09dabc2a16a03e1
Tobias Keller <kellertobs@gmail.com>
2023-02-13 17:15:44 +0100

    minor adjustment of convergence criteria in equilibrium solver

--- src/equilibrium.m
+++ src/equilibrium.m
@@ -17,11 +17,11 @@ perCm = (perCm-cphs0)./(cphs1-cphs0);
 perCx = (perCx-cphs0)./(cphs1-cphs0);
 
 iter    = 1;
-maxit   = 50;
+maxit   = 75;
 resnorm = 1;
 tol     = 1e-12;
 alpha   = 0.50;
-beta    = 0.25;
+beta    = 0.10;
 xi = xq; fi = fq;
 
 vmq_c0 = (4.7773e-7.*P.^0.6 + 1e-11.*P) .* exp(2565*(1./(T0+273.15)-1./(perTd+273.15))); % Katz et al., 2003; Moore et al., 1998
@@ -84,7 +84,7 @@ end
 T   = max(0,min(1,(T0 - P*clap -Tphs0)./(Tphs1-Tphs0)));
 
 a = 20;
-b = 0.04;
+b = 0.05;
 ind1 = T>=perT+b;
 ind2 = T< perT-b;
 ind3 = T>=perT-b & T<perT+b;
