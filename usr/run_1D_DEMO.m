12a1ee724785e0e47b7b41c54bb39979d7a1304a
Tobias Keller <kellertobs@gmail.com>
2025-02-12 17:11:27 +0000

    update DEMO runs

--- usr/run_1D_DEMO.m
+++ usr/run_1D_DEMO.m
@@ -7,14 +7,14 @@ run('./par_default')
 % set run parameters
 runID    =  '1D_DEMO';           % run identifier
 restart  =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
-nop      =  100;                 % output frame plotted/saved every 'nop' time steps
+nop      =  200;                 % output frame plotted/saved every 'nop' time steps
 plot_op  =  1;                   % switch on to live plot results
 save_op  =  1;                   % switch on to save output to file
 plot_cv  =  0;                   % switch on to live plot iterative convergence
 
 % set model domain parameters
 D        =  10;                  % chamber depth [m]
-N        =  300;                 % number of grid points in z-direction
+N        =  400;                 % number of grid points in z-direction
 h        =  D/N;                 % grid spacing (equal in both dimensions, do not set) [m]
 L        =  h;                   % chamber width (equal to h for 1-D mode) [m]
 
@@ -26,7 +26,7 @@ dt       =  36;                  % initial time step [s]
 % set initial thermo-chemical state
 T0       =  1215;                % temperature top  layer [deg C]
 T1       =  T0;                  % temperature base layer [deg C]
-c0       =  [0.10  0.75  0.15  0.01];  % components (maj comp, H2O) top  layer [wt] (will be normalised to unit sum!)
+c0       =  [11  17  36  32  1  3  0.5]/100;  % components (maj comp, H2O) top  layer [wt] (will be normalised to unit sum!)
 c1       =  c0;                  % components (maj comp, H2O) base layer [wt] (will be normalised to unit sum!)
 dcr      =  [0,0,0,0,0,0,0];
 dcg      =  [0,0,0,0,0,0,0];
@@ -34,7 +34,7 @@ dcg      =  [0,0,0,0,0,0,0];
 % set thermo-chemical boundary parameters
 periodic =  1;
 bndmode  =  3;                   % boundary assimilation mode (0 = none; 1 = top only; 2 = bot only; 3 = top/bot only; 4 = all walls; 5 = only sides)
-bnd_w    =  0.1;                 % boundary layer width [m]
+bnd_w    =  D/100;               % boundary layer width [m]
 tau_T    =  (2*bnd_w)^2/1e-6;    % wall cooling/assimilation time [s]
 Twall    =  [300,300,nan];       % [top,bot,sds] wall rock temperature [degC] (nan = insulating)
 cwall    =  nan(3,4);
@@ -52,11 +52,9 @@ CFL      =  0.50;                % (physical) time stepping courant number (mult
 rtol     =  1e-4;                % outer its relative tolerance
 atol     =  1e-8;                % outer its absolute tolerance
 maxit    =  50;                  % maximum outer its
-Delta_cnv=  h/2;                 % correlation length for eddy, convection diffusivity (multiple of h, 0.5-1)
+Delta_cnv=  D/10;                % correlation length for eddy, convection diffusivity (multiple of h, 0.5-1)
 Delta_sgr=  dx0*10;              % correlation length for phase fluctuation diffusivity (multiple of dx0, df0, 10-20)
 
-lambda1 = 0;
-lambda2 = 0;
 
 %*****  RUN NAKHLA MODEL  *************************************************
 run('../src/main')
--- usr/run_2D_DEMO.m
+++ usr/run_2D_DEMO.m
@@ -6,7 +6,7 @@ run('./par_default')
 
 % set run parameters
 runID     =  '2D_DEMO';           % run identifier
-restart   = -1;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
+restart   =  0;                   % restart from file (0: new run; <1: restart from last; >1: restart from specified frame)
 nop       =  100;                 % output frame plotted/saved every 'nop' time steps
 plot_op   =  1;                   % switch on to live plot results
 save_op   =  1;                   % switch on to save output to file
@@ -14,22 +14,22 @@ plot_cv   =  0;                   % switch on to live plot iterative convergence
 
 % set model domain parameters
 D         =  10;                  % chamber depth [m]
-N         =  150;                 % number of grid points in z-direction
+N         =  200;                 % number of grid points in z-direction
 h         =  D/N;                 % grid spacing (equal in both dimensions, do not set) [m]
 L         =  D/2;                 % chamber width (equal to h for 1-D mode) [m]
 
 % set model timing parameters
 Nt        =  5e5;                 % number of time steps to take
 tend      =  1*yr;                % end time for simulation [s]
-dt        =  36;                  % initial time step [s]
+dt        =  1;                  % initial time step [s]
 
 % set initial thermo-chemical state
 T0        =  1215;                % temperature top  layer [deg C]
 T1        =  T0;                  % temperature base layer [deg C]
-c0        =  [0.10  0.75  0.15  0.01];  % components (maj comp, H2O) top  layer [wt] (will be normalised to unit sum!)
+c0        =  [11  17  36  32  1  3  0.5]/100;  % components (maj comp, H2O) top  layer [wt] (will be normalised to unit sum!)
 c1        =  c0;                  % components (maj comp, H2O) base layer [wt] (will be normalised to unit sum!)
-dcr       =  [1,-1/2,-1/2,0]*1e-4;
-dr_trc    =  [0,0,1,0,0,-1];      % trace elements random noise
+dcr       =  [1,1,1,-1,-1,-1,0]*1e-4;
+dr_trc    =  [1,1,1e3,-1,-1,-1e3]*1e-4;      % trace elements random noise
 
 % set thermo-chemical boundary parameters
 periodic  =  1;
@@ -50,8 +50,10 @@ TINT      =  'bd2im';             % time integration scheme ('be1im','bd2im','cn
 ADVN      =  'weno5';             % advection scheme ('centr','upw1','quick','fromm','weno3','weno5','tvdim')
 CFL       =  1.0;                 % (physical) time stepping courant number (multiplies stable step) [0,1]
 rtol      =  1e-4;                % outer its relative tolerance
-atol      =  1e-7;                % outer its absolute tolerance
+atol      =  1e-8;                % outer its absolute tolerance
 maxit     =  15;                  % maximum outer its
+Delta_cnv =  h;                   % correlation length for eddy, convection diffusivity (multiple of h, 0.5-1)
+Delta_sgr =  dx0*10;              % correlation length for phase fluctuation diffusivity (multiple of dx0, df0, 10-20)
 
 
 %*****  RUN NAKHLA MODEL  *************************************************
